ff69ff76e7eb55f25a7adf7947745761532f46f2
Le Juez Victor <victor.lejuez@protonmail.ch>
2023-08-05 18:07:42 +0200

    Refactoring of NativeLoader.java

--- app/src/main/java/com/raylib/raymob/NativeLoader.java
+++ app/src/main/java/com/raylib/raymob/NativeLoader.java
@@ -3,7 +3,6 @@ package com.raylib.raymob;  // Don't change the package name (see gradle.propert
 import android.app.NativeActivity;
 import android.os.Bundle;
 import android.view.View;
-//import android.util.Log;
 
 public class NativeLoader extends NativeActivity {
 
@@ -22,29 +21,26 @@ public class NativeLoader extends NativeActivity {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) setImmersiveMode();       // If the app has focus, re-enable immersive mode
+    }
 
-        // If the app has focus, re-enable immersive mode
-        if (hasFocus) {
-            setImmersiveMode();
-        }
-
+    // Set immersive mode method
+    // NOTE: The method is deprecated, you can use androidX instead
+    //       but this will greatly increase the size of the application
+    private void setImmersiveMode() {
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_FULLSCREEN              |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION         |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY        |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN       |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION  |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
     }
 
     // Static method get the instance (from C/C++ for example)
     public static NativeLoader getInstance() {
         return instance;
-    }
-
-    // Set immersive mode method
-    private void setImmersiveMode() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        );
     }
 
 }
