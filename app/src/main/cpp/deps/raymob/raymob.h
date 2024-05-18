e538335aebba474216d77896b2a7ad89636c30d5
Bigfoot71 <victor.lejuez@protonmail.ch>
2024-05-18 23:16:12 +0200

    Added better documentation

--- app/src/main/cpp/deps/raymob/raymob.h
+++ app/src/main/cpp/deps/raymob/raymob.h
@@ -37,47 +37,167 @@
 extern "C" {
 #endif
 
-// NOTE: This function is defined in 'raylib/platforms/rcore_android.c'.
-// The function is not public by default, so we are making its declaration
-// public here now (instead of doing it in 'raylib.h'), in order to keep a
-// clean version of raylib.
-
+/**
+ * @brief Retrieves the Android application object.
+ *
+ * This function is defined in 'raylib/platforms/rcore_android.c'.
+ * The function is not public by default, so we are making its declaration
+ * public here now (instead of doing it in 'raylib.h'), in order to keep a
+ * clean version of raylib.
+ *
+ * @return Pointer to the Android application object.
+ */
 RMBAPI struct android_app *GetAndroidApp(void);
 
 /* Helper functions */
 
-RMBAPI JNIEnv* AttachCurrentThread(void);                               // Attaches the current native thread to the Java VM environment.
-RMBAPI void DetachCurrentThread(void);                                  // Detaches the current native thread from the Java VM environment.
+/**
+ * @brief Attaches the current native thread to the Java VM environment.
+ *
+ * @return Pointer to the JNIEnv structure.
+ */
+RMBAPI JNIEnv* AttachCurrentThread(void);
 
-RMBAPI jobject GetNativeLoaderInstance(void);                           // Returns a pointer to the class that initiated the native activity.
-RMBAPI jobject GetFeaturesInstance(void);                               // Returns a pointer to the raymob features class.
+/**
+ * @brief Detaches the current native thread from the Java VM environment.
+ */
+RMBAPI void DetachCurrentThread(void);
 
-// WARNING: Return a string allocated on the heap.
-// The responsibility for releasing the channel lies with the user.
-RMBAPI char* GetCacheDir(void);                                         // Gets the cache directory path of the Android application.
+/**
+ * @brief Returns a pointer to the class that initiated the native activity.
+ *
+ * @return Pointer to the native loader instance.
+ */
+RMBAPI jobject GetNativeLoaderInstance(void);
+
+/**
+ * @brief Returns a pointer to the raymob features class.
+ *
+ * @return Pointer to the features instance.
+ */
+RMBAPI jobject GetFeaturesInstance(void);
+
+/**
+ * @brief Gets the cache directory path of the Android application.
+ *
+ * @warning This function returns a string allocated on the heap.
+ * The responsibility for releasing the memory lies with the user.
+ *
+ * @return Pointer to the cache directory path string.
+ */
+RMBAPI char* GetCacheDir(void);
 
 /* Feature functions */
 
-RMBAPI void Vibrate(float sec);                                         // Initiates device vibration for the specified duration in seconds.
+/**
+ * @brief Initiates device vibration for the specified duration in seconds.
+ *
+ * @param sec Duration of vibration in seconds.
+ */
+RMBAPI void Vibrate(float sec);
 
-RMBAPI void StartAccelerometerListening(void);                          // Starts listening to accelerometer sensor data.
-RMBAPI void StopAccelerometerListening(void);                           // Stops listening to accelerometer sensor data.
-RMBAPI Vector3 GetAccelerometerAxis(void);                              // Returns the current accelerometer axis vector.
-RMBAPI float GetAccelerometerX(void);                                   // Returns the current value of the X-axis accelerometer.
-RMBAPI float GetAccelerometerY(void);                                   // Returns the current value of the Y-axis accelerometer.
-RMBAPI float GetAccelerometerZ(void);                                   // Returns the current value of the Z-axis accelerometer.
+/**
+ * @brief Starts listening to accelerometer sensor data.
+ */
+RMBAPI void StartAccelerometerListening(void);
 
-RMBAPI void ShowSoftKeyboard(void);                                     // Displays the soft keyboard on the screen.
-RMBAPI void HideSoftKeyboard(void);                                     // Hides the soft keyboard from the screen.
-RMBAPI bool IsSoftKeyboardActive(void);                                 // Checks if the soft keyboard is currently active.
-RMBAPI int GetLastSoftKeyCode(void);                                    // Returns the code of the last key pressed on the soft keyboard.
-RMBAPI unsigned short GetLastSoftKeyLabel(void);                        // Returns the label of the last key pressed on the soft keyboard.
-RMBAPI int GetLastSoftKeyUnicode(void);                                 // Returns the Unicode value of the last key pressed on the soft keyboard.
-RMBAPI char GetLastSoftKeyChar(void);                                   // Returns the character value of the last key pressed on the soft keyboard.
-RMBAPI void ClearLastSoftKey(void);                                     // Clears the record of the last soft key pressed.
-RMBAPI void SoftKeyboardEditText(char* text, unsigned int size);        // Allows editing the text displayed in the soft keyboard.
+/**
+ * @brief Stops listening to accelerometer sensor data.
+ */
+RMBAPI void StopAccelerometerListening(void);
 
-RMBAPI void KeepScreenOn(bool keepOn);                                  // Controls whether the screen should remain on or not.
+/**
+ * @brief Returns the current accelerometer axis vector.
+ *
+ * @return Current accelerometer axis vector.
+ */
+RMBAPI Vector3 GetAccelerometerAxis(void);
+
+/**
+ * @brief Returns the current value of the X-axis accelerometer.
+ *
+ * @return Current value of the X-axis accelerometer.
+ */
+RMBAPI float GetAccelerometerX(void);
+
+/**
+ * @brief Returns the current value of the Y-axis accelerometer.
+ *
+ * @return Current value of the Y-axis accelerometer.
+ */
+RMBAPI float GetAccelerometerY(void);
+
+/**
+ * @brief Returns the current value of the Z-axis accelerometer.
+ *
+ * @return Current value of the Z-axis accelerometer.
+ */
+RMBAPI float GetAccelerometerZ(void);
+
+/**
+ * @brief Displays the soft keyboard on the screen.
+ */
+RMBAPI void ShowSoftKeyboard(void);
+
+/**
+ * @brief Hides the soft keyboard from the screen.
+ */
+RMBAPI void HideSoftKeyboard(void);
+
+/**
+ * @brief Checks if the soft keyboard is currently active.
+ *
+ * @return True if the soft keyboard is active, false otherwise.
+ */
+RMBAPI bool IsSoftKeyboardActive(void);
+
+/**
+ * @brief Returns the code of the last key pressed on the soft keyboard.
+ *
+ * @return Code of the last key pressed on the soft keyboard.
+ */
+RMBAPI int GetLastSoftKeyCode(void);
+
+/**
+ * @brief Returns the label of the last key pressed on the soft keyboard.
+ *
+ * @return Label of the last key pressed on the soft keyboard.
+ */
+RMBAPI unsigned short GetLastSoftKeyLabel(void);
+
+/**
+ * @brief Returns the Unicode value of the last key pressed on the soft keyboard.
+ *
+ * @return Unicode value of the last key pressed on the soft keyboard.
+ */
+RMBAPI int GetLastSoftKeyUnicode(void);
+
+/**
+ * @brief Returns the character value of the last key pressed on the soft keyboard.
+ *
+ * @return Character value of the last key pressed on the soft keyboard.
+ */
+RMBAPI char GetLastSoftKeyChar(void);
+
+/**
+ * @brief Clears the record of the last soft key pressed.
+ */
+RMBAPI void ClearLastSoftKey(void);
+
+/**
+ * @brief Allows editing the text displayed in the soft keyboard.
+ *
+ * @param text Pointer to the text to be edited.
+ * @param size Size of the text buffer.
+ */
+RMBAPI void SoftKeyboardEditText(char* text, unsigned int size);
+
+/**
+ * @brief Controls whether the screen should remain on or not.
+ *
+ * @param keepOn If true, the screen should remain on; otherwise, it should not.
+ */
+RMBAPI void KeepScreenOn(bool keepOn);
 
 #if defined(__cplusplus)
 }
