3baf6f51e58b556934663654b4e4c5928f1dbc80
RugiSerl <cooletouf7@gmail.com>
2024-11-24 16:31:17 +0100

    Added gyroscope support

--- app/src/main/java/com/raylib/raymob/Features.java
+++ app/src/main/java/com/raylib/raymob/Features.java
@@ -33,7 +33,7 @@ public class Features {
     /* FEATURES INSTANCES */
 
     private final Optional<Vibration> vibrator;
-    private final Optional<Accelerometer> accelerometer;
+    private final Optional<Sensor> sensor;
     private final Optional<SoftKeyboard> softKeyboard;
     private final Optional<DisplayManager> display;
 
@@ -41,7 +41,7 @@ public class Features {
 
     public Features(Context context) {
         vibrator = BuildConfig.FEATURE_VIBRATION ? Optional.of(new Vibration(context)) : Optional.empty();
-        accelerometer = BuildConfig.FEATURE_ACCELEROMETER ? Optional.of(new Accelerometer(context)) : Optional.empty();
+        sensor = BuildConfig.FEATURE_ACCELEROMETER ? Optional.of(new Sensor(context)) : Optional.empty();
         softKeyboard = BuildConfig.FEATURE_SOFT_KEYBOARD ? Optional.of(new SoftKeyboard(context)) : Optional.empty();
         display = BuildConfig.FEATURE_DISPLAY ? Optional.of(new DisplayManager(context)) : Optional.empty();
     }
@@ -52,26 +52,40 @@ public class Features {
         vibrator.ifPresent(v -> v.vibrate(seconds));
     }
 
+    /* SENSOR */
+
+    public void startSensorListening() {
+        sensor.ifPresent(Sensor::startListening);
+    }
+
+    public void stopSensorListening() {
+        sensor.ifPresent(Sensor::stopListening);
+    }
+
     /* ACCELEROMETER */
-
-    public void startAccelerometerListening() {
-        accelerometer.ifPresent(Accelerometer::startListening);
-    }
-
-    public void stopAccelerometerListening() {
-        accelerometer.ifPresent(Accelerometer::stopListening);
-    }
-
     public float getAccelerometerX() {
-        return accelerometer.map(Accelerometer::getX).orElse(0f);
+        return sensor.map(Sensor::getAccelerometerX).orElse(0f);
     }
 
     public float getAccelerometerY() {
-        return accelerometer.map(Accelerometer::getY).orElse(0f);
+        return sensor.map(Sensor::getAccelerometerY).orElse(0f);
     }
 
     public float getAccelerometerZ() {
-        return accelerometer.map(Accelerometer::getZ).orElse(0f);
+        return sensor.map(Sensor::getAccelerometerZ).orElse(0f);
+    }
+
+    /* GYROSCOPE */
+    public float getGyroscopeX() {
+        return sensor.map(Sensor::getGyroscopeX).orElse(0f);
+    }
+
+    public float getGyroscopeY() {
+        return sensor.map(Sensor::getGyroscopeY).orElse(0f);
+    }
+
+    public float getGyroscopeZ() {
+        return sensor.map(Sensor::getGyroscopeZ).orElse(0f);
     }
 
     /* SOFT KEYBOARD */
